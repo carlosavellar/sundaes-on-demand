9b9651edf4d24cbf8c6792625182eb1de959cbe2
carlos.avellar <carlos.avellar@gmail.com>
2023-01-11 20:01:00 -0300

    Setup mock server

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server.js';
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// ANY NETWORK REQUEST THAT COMES THROUGH ROUTE THEM TO MOCK SERVER WORKER
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+
+afterEach(() => server.resetHandlers());
+//HERE WE RESET THE HANDLERS
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
